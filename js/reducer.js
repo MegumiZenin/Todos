a05d53bdc9b3aaf65217729f5ac772eb9c803547
Set <110546945+MegumiZenin@users.noreply.github.com>
2023-02-18 21:32:27 +0700

    Add files via upload

---
+++ js/reducer.js
@@ -0,0 +1,59 @@
+import Storage from "../storage.js";
+
+const init = {
+    todos: Storage.get(),
+    filter: 'all',
+    filters: {
+        all: () => true,
+        active: (todo) => !todo.completed,
+        completed: (todo) => todo.completed
+    },
+    editIndex: null,
+}
+
+const actions = {
+    add({todos},...args){
+        todos.unshift({content: args, completed: false});
+        Storage.set(todos)
+    },
+    delete({todos},index) {
+        todos.splice(index, 1);
+        Storage.set(todos)
+    },
+    check({todos},index) {
+        todos[index].completed = !todos[index].completed;
+        Storage.set(todos)
+    },
+    checkAll({todos}, checked) {
+        todos.forEach(todo => todo.completed = checked);
+        Storage.set(todos)
+    },
+    clearCompleted(state) {
+        state.todos = state.todos.filter(todo => !todo.completed);
+        Storage.set(state.todos)
+    },
+    setFilter(state, type) {
+        state.filter = type;
+    },
+    startEdit(state, index) {
+        state.editIndex = index;
+    },
+    endEdit(state, value) {
+        if (value) {
+            state.todos[state.editIndex].content = value;
+            Storage.set(state.todos)
+        } else {
+            this.delete(state, state.editIndex);
+        }
+        state.editIndex = null;
+    },
+    cancelEdit(state) {
+        state.editIndex = null;
+    }
+
+}
+
+export default function reducer(state = init, action, ...args) {
+    actions[action] && actions[action](state,...args);
+    return state;
+}
